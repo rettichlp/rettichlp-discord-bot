e10e7e6caa965ab41df9dd910ab8445cc2b3e7c6
RettichLP <rettichlp01@gmail.com>
2025-09-11 23:19:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# RettichLP Discord Bot
+
+Ich bin Kohlrabi, ein kleiner Helfer und Aufpasser. 🥦
+Ich sorge für Ordnung, helfe bei Rollen & Channels und bringe ab und zu ein bisschen Spaß in den Chat! 🪴
---
+++ src/main/java/de/rettichlp/discordbot/Application.java
@@ -0,0 +1,46 @@
+package de.rettichlp.discordbot;
+
+import lombok.extern.log4j.Log4j2;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static java.lang.System.currentTimeMillis;
+import static java.lang.System.getenv;
+import static net.dv8tion.jda.api.requests.GatewayIntent.GUILD_MEMBERS;
+import static net.dv8tion.jda.api.requests.GatewayIntent.GUILD_MESSAGES;
+import static net.dv8tion.jda.api.requests.GatewayIntent.GUILD_VOICE_STATES;
+import static net.dv8tion.jda.api.requests.GatewayIntent.MESSAGE_CONTENT;
+import static net.dv8tion.jda.api.utils.Compression.NONE;
+import static net.dv8tion.jda.api.utils.cache.CacheFlag.MEMBER_OVERRIDES;
+import static org.springframework.boot.SpringApplication.run;
+
+@Log4j2
+@SpringBootApplication
+public class Application {
+
+    public static JDA BOT;
+
+    public static void main(String[] args) {
+        run(Application.class, args);
+
+        long discordBotStartTime = currentTimeMillis();
+        log.info("Discord bot starting");
+        startDiscordBot();
+        log.info("Discord bot started in {}ms", currentTimeMillis() - discordBotStartTime);
+    }
+
+    private static void startDiscordBot() {
+        BOT = JDABuilder
+                .createDefault("DISCORD_BOT_TOKEN")
+                .disableCache(MEMBER_OVERRIDES) // Disable parts of the cache
+                .setBulkDeleteSplittingEnabled(false) // Enable the bulk delete event
+                .setCompression(NONE) // Disable compression (not recommended)
+                .enableIntents(MESSAGE_CONTENT)
+                .enableIntents(GUILD_MEMBERS)
+                .enableIntents(GUILD_MESSAGES)
+                .enableIntents(GUILD_VOICE_STATES)
+                .addEventListeners()
+                .build();
+    }
+}
