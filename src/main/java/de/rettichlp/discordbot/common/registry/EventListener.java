29026d7f4a688a35c37b64244886687923f8b3a8
RettichLP <rettichlp01@gmail.com>
2025-09-12 12:37:42 +0200

    Add retention and target annotations to EventListener for improved metadata

--- src/main/java/de/rettichlp/discordbot/common/registry/EventListener.java
+++ src/main/java/de/rettichlp/discordbot/common/registry/EventListener.java
@@ -2,6 +2,14 @@ package de.rettichlp.discordbot.common.registry;
 
 import org.atteo.classindex.IndexAnnotated;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@Target(TYPE)
 @IndexAnnotated
 public @interface EventListener {
 
