0eb0bba1929448c2c3072cf0e9db67e969dd1702
RettichLP <rettichlp01@gmail.com>
2025-09-12 12:38:05 +0200

    Enhance listener registration with success and skip tracking

--- src/main/java/de/rettichlp/discordbot/common/registry/Registry.java
+++ src/main/java/de/rettichlp/discordbot/common/registry/Registry.java
@@ -4,15 +4,21 @@ import lombok.extern.log4j.Log4j2;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static de.rettichlp.discordbot.Application.discordBot;
+import static java.util.stream.StreamSupport.stream;
 import static org.atteo.classindex.ClassIndex.getAnnotated;
 
 @Log4j2
 public class Registry {
 
     public void registerListeners() {
-        getAnnotated(EventListener.class).forEach(listenerClass -> {
+        AtomicInteger successfulRegistrations = new AtomicInteger();
+        AtomicInteger skippedRegistrations = new AtomicInteger();
+
+        Iterable<Class<?>> listenerClasses = getAnnotated(EventListener.class);
+        listenerClasses.forEach(listenerClass -> {
             try {
                 EventListener annotation = listenerClass.getAnnotation(EventListener.class);
                 if (annotation.skipped()) {
@@ -22,9 +28,12 @@ public class Registry {
 
                 ListenerAdapter listenerInstance = (ListenerAdapter) listenerClass.getConstructor().newInstance();
                 discordBot.addEventListener(listenerInstance);
+                successfulRegistrations.getAndIncrement();
             } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
                 log.error("Failed to register listener: {}", listenerClass.getName(), e);
             }
         });
+
+        log.info("Registered {}/{} event listeners ({} skipped)", successfulRegistrations.get(), stream(listenerClasses.spliterator(), false).count(), skippedRegistrations.get());
     }
 }
