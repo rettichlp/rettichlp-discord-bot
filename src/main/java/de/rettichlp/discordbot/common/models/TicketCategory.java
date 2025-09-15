c9d9852a1d20d67b03846aa520445f1c3ec8771d
RettichLP <rettichlp01@gmail.com>
2025-09-15 12:19:53 +0200

    Add secretary role permission handling for ticket creation

--- src/main/java/de/rettichlp/discordbot/common/models/TicketCategory.java
+++ src/main/java/de/rettichlp/discordbot/common/models/TicketCategory.java
@@ -6,14 +6,18 @@ import net.dv8tion.jda.api.components.actionrow.ActionRow;
 import net.dv8tion.jda.api.components.buttons.Button;
 import net.dv8tion.jda.api.components.textinput.TextInput;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.interactions.callbacks.IReplyCallback;
 import net.dv8tion.jda.api.interactions.modals.Modal;
+import net.dv8tion.jda.api.requests.restaction.ChannelAction;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.EnumSet;
 
 import static de.rettichlp.discordbot.Application.discordBotProperties;
+import static java.util.Objects.nonNull;
 import static net.dv8tion.jda.api.Permission.VIEW_CHANNEL;
 import static net.dv8tion.jda.api.components.buttons.Button.secondary;
 import static net.dv8tion.jda.api.components.buttons.Button.success;
@@ -25,13 +29,14 @@ import static net.dv8tion.jda.api.interactions.modals.Modal.create;
 @AllArgsConstructor
 public enum TicketCategory {
 
-    GENERALLY("ticket_create", "Allgemeines Ticket", fromUnicode("U+1F3AB")),
-    PKUTILS("ticket_create_pkutils", "Ticket (PKUtils)", fromUnicode("U+1F9E9")),
-    RLPTP("ticket_create_rlptp", "Ticket (RLPTP)", fromUnicode("U+1F5BC"));
+    GENERALLY("ticket_create", "Allgemeines Ticket", fromUnicode("U+1F3AB"), null),
+    PKUTILS("ticket_create_pkutils", "Ticket (PKUtils)", fromUnicode("U+1F9E9"), discordBotProperties.getGuild().getRoleById("1413069992123170877")),
+    RLPTP("ticket_create_rlptp", "Ticket (RLPTP)", fromUnicode("U+1F5BC"), discordBotProperties.getGuild().getRoleById("1417089576706641920"));
 
     private final String id;
     private final String buttonLabel;
     private final Emoji emoji;
+    private final Role secretaryRole;
 
     @NotNull
     public Button getTicketCreateButton() {
@@ -56,19 +61,19 @@ public enum TicketCategory {
         String userName = member.getUser().getName();
         String memberId = member.getId();
 
-//        Role supporterRole = discordBotProperties.getSupporterRole(); TODO
-//        Role moderatorRole = discordBotProperties.getModeratorRole();
-        discordBotProperties.getTicketCategory().createTextChannel("ticket-" + userName)
+        ChannelAction<TextChannel> textChannelChannelAction = discordBotProperties.getTicketCategory().createTextChannel("ticket-" + userName)
                 .setTopic("Ticket von " + userName + " (" + memberId + ")")
                 .addPermissionOverride(discordBotProperties.getGuild().getPublicRole(), null, EnumSet.of(VIEW_CHANNEL))
-                .addPermissionOverride(member, EnumSet.of(VIEW_CHANNEL), null)
-//                .addPermissionOverride(discordBotProperties.getSeniorModeratorRole(), EnumSet.of(VIEW_CHANNEL), null)
-//                .addPermissionOverride(moderatorRole, EnumSet.of(VIEW_CHANNEL), null)
-//                .addPermissionOverride(supporterRole, EnumSet.of(VIEW_CHANNEL), null)
-                .queue(textChannel -> textChannel
-                        .sendMessage("Hey " + member.getAsMention() + "! Danke, dass du ein " + this.buttonLabel + " erstellt hast. Das Ticket wird schnellstmöglich bearbeitet.\n"
-                                + "Anliegen: " + topic)
-                        .addComponents(ActionRow.of(success("btn_ticket_close", "Ticket schließen").withEmoji(fromUnicode("U+1F512"))))
-                        .queue(message -> event.reply("Du hast ein Ticket erstellt: " + message.getJumpUrl()).setEphemeral(true).queue()));
+                .addPermissionOverride(member, EnumSet.of(VIEW_CHANNEL), null);
+
+        if (nonNull(this.secretaryRole)) {
+            textChannelChannelAction = textChannelChannelAction.addPermissionOverride(this.secretaryRole, EnumSet.of(VIEW_CHANNEL), null);
+        }
+
+        textChannelChannelAction.queue(textChannel -> textChannel
+                    .sendMessage("Hey " + member.getAsMention() + "! Danke, dass du ein " + this.buttonLabel + " erstellt hast. Das Ticket wird schnellstmöglich bearbeitet.\n" +
+                            "Anliegen: " + topic + (nonNull(this.secretaryRole) ? "\n" + this.secretaryRole.getAsMention() : ""))
+                    .addComponents(ActionRow.of(success("btn_ticket_close", "Ticket schließen").withEmoji(fromUnicode("U+1F512"))))
+                    .queue(message -> event.reply("Du hast ein Ticket erstellt: " + message.getJumpUrl()).setEphemeral(true).queue()));
     }
 }
