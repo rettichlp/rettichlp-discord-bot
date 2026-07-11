33dc084229f75d73e280e2f2705b1daa34289287
RettichLP <rettichlp01@gmail.com>
2026-07-11 22:19:47 +0200

    Refactor `TicketCategory` enum to update category definitions and simplify ticket creation logic

--- src/main/java/de/rettichlp/discordbot/common/models/TicketCategory.java
+++ src/main/java/de/rettichlp/discordbot/common/models/TicketCategory.java
@@ -2,79 +2,28 @@ package de.rettichlp.discordbot.common.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import net.dv8tion.jda.api.components.actionrow.ActionRow;
-import net.dv8tion.jda.api.components.buttons.Button;
-import net.dv8tion.jda.api.components.label.Label;
-import net.dv8tion.jda.api.components.textinput.TextInput;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.components.selections.SelectOption;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
-import net.dv8tion.jda.api.interactions.callbacks.IReplyCallback;
-import net.dv8tion.jda.api.modals.Modal;
-import net.dv8tion.jda.api.requests.restaction.ChannelAction;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.EnumSet;
-
-import static de.rettichlp.discordbot.Application.discordBotProperties;
-import static java.util.Objects.nonNull;
-import static net.dv8tion.jda.api.Permission.VIEW_CHANNEL;
-import static net.dv8tion.jda.api.components.buttons.Button.secondary;
-import static net.dv8tion.jda.api.components.buttons.Button.success;
-import static net.dv8tion.jda.api.components.textinput.TextInputStyle.PARAGRAPH;
-import static net.dv8tion.jda.api.entities.emoji.Emoji.fromUnicode;
-import static net.dv8tion.jda.api.modals.Modal.create;
+import org.jspecify.annotations.NonNull;
 
 @Getter
 @AllArgsConstructor
 public enum TicketCategory {
 
-    GENERALLY("ticket_create", "Allgemeines Ticket", fromUnicode("U+1F3AB"), null),
-    PKUTILS("ticket_create_pkutils", "Ticket (PKUtils)", fromUnicode("U+1F9E9"), discordBotProperties.getGuild().getRoleById("1413069992123170877")),
-    RLPTP("ticket_create_rlptp", "Ticket (RLPTP)", fromUnicode("U+1F5BC"), discordBotProperties.getGuild().getRoleById("1417089576706641920"));
+    THE_RETTINGTON_COMPANION("The Rettington Companion", "the-rettington-companion", "Eine Mod mit nützlichen Hilfsmitteln für einen besseren Minecraft Alltag.", Emoji.fromFormatted("<:therettingtonicon:1501507423888736426>")),
+    THE_RETTINGTON_AESTHETICS("The Rettington Aesthetics", "the-rettington-aesthetics", "Ein Minecraft-Ressourcenpaket, das Elemente von BetterVanillaBuilding und Excalibur kombiniert.", Emoji.fromFormatted("<:therettingtonicon:1501507423888736426>")),
+    THE_RETTINGTON_CONCIERGE("The Rettington Concierge", "the-rettington-concierge", "Eine Java Bibliothek für Minecraft Plugins.", Emoji.fromFormatted("<:java:1501512844137599086>")),
+    UCUTILS("UCUtils", "ucutils", "Eine Mod für den UnicaCity-Rollenspielserver mit nützliche Befehlen und Funktionen.", Emoji.fromFormatted("<:unicacity:1501509954555744276>"));
 
-    private final String id;
-    private final String buttonLabel;
+    private final String label;
+    private final String value;
+    private final String description;
     private final Emoji emoji;
-    private final Role secretaryRole;
+    //private final Role secretaryRole;
 
-    @NotNull
-    public Button getTicketCreateButton() {
-        return this == GENERALLY
-                ? success("btn_" + this.id, this.buttonLabel).withEmoji(this.emoji)
-                : secondary("btn_" + this.id, this.buttonLabel).withEmoji(this.emoji);
-    }
-
-    @NotNull
-    public Modal getTicketModal() {
-        TextInput logInput = TextInput.create("tip_topic", PARAGRAPH)
-                .setRequired(true)
-                .setPlaceholder("Hey, ich brauche bitte Hilfe bei ...")
-                .build();
-
-        return create("mdl_" + this.id, "Neues Ticket")
-                .addComponents(Label.of("Anliegen", logInput))
-                .build();
-    }
-
-    public void createTicketChannel(IReplyCallback event, @NotNull Member member, String topic) {
-        String userName = member.getUser().getName();
-        String memberId = member.getId();
-
-        ChannelAction<TextChannel> textChannelChannelAction = discordBotProperties.getTicketCategory().createTextChannel("ticket-" + userName)
-                .setTopic("Ticket von " + userName + " (" + memberId + ")")
-                .addPermissionOverride(discordBotProperties.getGuild().getPublicRole(), null, EnumSet.of(VIEW_CHANNEL))
-                .addPermissionOverride(member, EnumSet.of(VIEW_CHANNEL), null);
-
-        if (nonNull(this.secretaryRole)) {
-            textChannelChannelAction = textChannelChannelAction.addPermissionOverride(this.secretaryRole, EnumSet.of(VIEW_CHANNEL), null);
-        }
-
-        textChannelChannelAction.queue(textChannel -> textChannel
-                .sendMessage("Hey " + member.getAsMention() + "! Danke, dass du ein " + this.buttonLabel + " erstellt hast. Das Ticket wird schnellstmöglich bearbeitet.\n" +
-                        "Anliegen: " + topic + (nonNull(this.secretaryRole) ? "\n" + this.secretaryRole.getAsMention() : ""))
-                .addComponents(ActionRow.of(success("btn_ticket_close", "Ticket schließen").withEmoji(fromUnicode("U+1F512"))))
-                .queue(message -> event.reply("Du hast ein Ticket erstellt: " + message.getJumpUrl()).setEphemeral(true).queue()));
+    public @NonNull SelectOption getSelectOption() {
+        return SelectOption.of(this.label, this.value)
+                .withDescription(this.description)
+                .withEmoji(this.emoji);
     }
 }
